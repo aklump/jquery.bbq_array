fd8e0d58566021ca983c204c40f13cfe4228ad07
aklump <sourcecode@intheloftstudios.com>
2016-01-16 18:49:10 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+## jQuery BBQ Array
+
+The is an extension to  [jQuery BBQ](http://benalman.com/projects/jquery-bbq-plugin/) which allows you to push array elements onto states that are arrays.
+
+## How to use
+This is an example of how to use, give the following url:
+
+        index.php#foo[0]=bar&foo[1]=baz
+
+To remove `baz` do this:
+
+        $.bbq.removeArrayState('foo', 'baz');
+        
+To set `qux` as a value in `foo`:
+
+        $.bbq.setArrayState('foo', 'qux');
+
+## Will not duplicate
+If you call the same twice, it will NOT result in two new values being added
+
+        $.bbq.setArrayState('foo', 'qux');
+        $.bbq.setArrayState('foo', 'qux');
+        
+Will result in this url:
+
+        index.php#foo[0]=qux
+
+## Contact
+* **In the Loft Studios**
+* Aaron Klump - Developer
+* PO Box 29294 Bellingham, WA 98228-1294
+* _aim_: theloft101
+* _skype_: intheloftstudios
+* _d.o_: aklump
+* <http://www.InTheLoftStudios.com>
---
+++ jquery.bbq_array.js
@@ -0,0 +1,67 @@
+/**
+ * BBQ for Arrays jQuery Plugin v0.0.1
+ * http://www.intheloftstudios.com/packages/js/bbq_array
+ *
+ * Extends jQuery BBQ so that you can push states onto arrays.
+ *
+ * Copyright 2016, Aaron Klump <sourcecode@intheloftstudios.com>
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: Sat Jan 16 18:47:14 PST 2016
+ *
+ * @see http://benalman.com/projects/jquery-bbq-plugin/
+ */
+
+/**
+ * This is an example of how to use, give the following url:
+ *    index.php#foo[0]=bar&foo[1]=baz
+ *
+ * To remove baz do this:
+ *    $.bbq.removeArrayState('foo', 'baz');
+ *
+ * To set qux as a value in foo:
+ *    $.bbq.setArrayState('foo', 'qux');*
+ */
+;(function ($,window) {
+  "use strict";
+
+  /**
+   * Does the heavy lifting of reading the array and mergin our value.
+   * @param op One of: push or remove.
+   * @param key The string of the array name, if the url is foo[0]=bar, the key is 'foo'.
+   * @param value The value of the element of the array.  From above it would be 'bar'.
+   */
+  function setStateOfArray(op, key, value) {
+    // First get the state of the current array.
+    var args = $.deparam.fragment();
+    var state = args[key] || [];
+
+    // Determine if the value is already present in the array.
+    var pos = state.findIndex(function (element) {
+      return element === value;
+    });
+
+    // If it's not present then we'll add it and push the entire array as the new state.
+    var update = {};
+    if (op === 'push' && pos === -1) {
+      state.push(value);
+      update[key] = state;
+    }
+    else if (op === 'remove' && pos !== -1) {
+      state.splice(pos, 1);
+      update[key] = state;
+    }
+    if (update !== {}) {
+      $.bbq.pushState(update);
+    }
+  }
+
+  $.bbq = $.bbq || {};
+  $.bbq.pushArrayState = function (key, value) {
+    setStateOfArray('push', key, value);
+  };
+  $.bbq.removeArrayState = function (key, value) {
+    setStateOfArray('remove', key, value);
+  };
+
+})(jQuery,this);
