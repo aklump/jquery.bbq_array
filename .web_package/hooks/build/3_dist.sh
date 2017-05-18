192cf0ed4b7adeb3f1d8a456cc5708ef65679b79
Aaron Klump <sourcecode@intheloftstudios.com>
2017-05-18 10:19:59 -0700

    update dist step.

--- .web_package/hooks/build/3_dist.sh
+++ .web_package/hooks/build/3_dist.sh
@@ -3,7 +3,11 @@
 # @file
 # Copy distribution files to /dist
 # 
-sleep 3
+# Allow time for all CodeKit to minify.
+while [ ! -f "$7/jquery.bbq_array.min.js" ]; do
+  sleep 1
+done
+
 test -d "$7/dist" || mkdir -p "$7/dist"
 cp "$7/jquery.bbq_array.js" "$7/dist/"
 cp "$7/jquery.bbq_array.min.js" "$7/dist/"
